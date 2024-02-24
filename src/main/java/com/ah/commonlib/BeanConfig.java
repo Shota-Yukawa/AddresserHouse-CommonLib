ad8e653bc55f9c0ca012c6277a68b1638ab98de4
Shota-Yukawa <s.progdes@gmail.com>
2024-02-24 14:38:19 +0900

    ObjectMapperのLocalDateTimeのデ/シリアライズをカスタマイズし、JsonConverterが共通Beanを使える様にコンポーネント化'

--- src/main/java/com/ah/commonlib/BeanConfig.java
+++ src/main/java/com/ah/commonlib/BeanConfig.java
@@ -1,10 +1,19 @@
 package com.ah.commonlib;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
 @Configuration
 public class BeanConfig {
@@ -27,4 +36,21 @@ public class BeanConfig {
 		return modelMapper;
 	}
 
+	@Bean
+	ObjectMapper objectMapper() {
+		// ObjectMapperのインスタンスを作成
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		objectMapper.registerModule(new JavaTimeModule());
+
+		// LocalDateTimeのシリアライザとデシリアライザをカスタマイズ
+		SimpleModule simpleModule = new SimpleModule();
+		simpleModule.addSerializer(LocalDateTime.class,
+				new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")));
+		simpleModule.addDeserializer(LocalDateTime.class,
+				new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")));
+		objectMapper.registerModule(simpleModule);
+
+		return objectMapper;
+	}
 }
--- src/main/java/com/ah/commonlib/JsonConverter.java
+++ src/main/java/com/ah/commonlib/JsonConverter.java
@@ -1,12 +1,22 @@
 package com.ah.commonlib;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@Component
 public class JsonConverter {
+	
+	private final ObjectMapper objectMapper;
+	
+	@Autowired
+	public JsonConverter(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
+		
+	}
 
-	private static final ObjectMapper objectMapper = new ObjectMapper();
-
-	public static <T> T deserializeJson(Object jsonObject, Class<T> targetClass) {
+	public <T> T deserializeJson(Object jsonObject, Class<T> targetClass) {
 		return objectMapper.convertValue(jsonObject, targetClass);
 	}
 }
